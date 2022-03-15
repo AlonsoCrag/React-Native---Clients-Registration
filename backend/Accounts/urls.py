f73d90d6db795a55fef6e6e71bac7a5fba4538f2
AlonsoCrag <94062255+AlonsoCrag@users.noreply.github.com>
2022-03-15 03:48:48 -0600

    Delete backend directory

--- backend/Accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Test),
-    path('api/v1/register', views.Register, name="Route to register users"),
-    path('api/v1/login', views.Login, name="Route to log users"),
-]
--- backend/Serializer/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from Accounts.models import Account
-
-# Pass data to serialize, like QuerySets from models
-class SerializeData(ModelSerializer):
-    class Meta:
-        model = Account
-        fields = ["Username", "Email","Password", "Salt", "Genere"]
